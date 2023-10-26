b0f422d242d5944f29419223ac7642d160ea4d0d
Mikhail <lissen.ku6@gmail.com>
2023-10-27 02:56:14 +0300

    add new gcd game

--- brain_games/common_func.py
+++ brain_games/common_func.py
@@ -4,7 +4,7 @@ from random import randint
 def welcome_user() -> str:
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print(f'Welcome, {name}')
+    print(f'Hello, {name}!')
     return name
 
 
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from brain_games.common_func import welcome_user, random_num
+
+
+def main():
+    brain_gcd()
+
+def brain_gcd():
+    user_name = welcome_user()
+    right_answer_count = 0
+    print('Find the greatest common divisor of given numbers.')
+
+    while right_answer_count < 3:
+        a = random_num()
+        b = random_num()
+        a_div = []
+        b_div = []
+        print(f'Question: {a} {b}')
+
+        for i in range(1, a + 1):
+            a_div.append(a / i)
+        for i in range(1, b + 1):
+            b_div.append(b / i)
+
+        biggest_common_division = int(max(list(set(a_div).intersection(set(b_div)))))
+        user_answer = int(input("Your answer: "))
+
+        if user_answer == biggest_common_division:
+            print('Correct!')
+            right_answer_count += 1
+        else:
+            print(f"'{user_answer}' is wrong answer;(.Correct answer was '{biggest_common_division}'.\nLet's try again, {user_name}!")
+            break
+    else:
+        print(f'Congratulations, {user_name}!')
+
+
+if __name__ == '__main__':
+    main()
